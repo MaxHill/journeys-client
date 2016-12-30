b03fdedd0048b385ac8ef0397083fe4c8e48dd13
Max Hill <max@hilloco.se>
2016-12-30 16:17:16 +0100

    :art: Don't double include Vuex

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import Vue from 'vue';
-import Vuex from 'vuex';
 import App from './App';
 import VueRouter from 'vue-router';
 import routes from './routes';
 import store from './store/store';
 
 Vue.use(VueRouter);
-Vue.use(Vuex);
 
 const router = new VueRouter({
     mode: 'history',
